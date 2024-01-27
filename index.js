d7c5f4e2f8d6ceab83a5c9ed9dd094cceec7248b
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-01-26 23:49:59 -0600

    index.js

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i <numberOfDrumButtons; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert ("I got clicked!");
-    //add more funciton uses
+    alert ("I got clicked!");                              //pop up on page when clicked
+    
+    this.style.color = "white";                            // changes the clicked drums white
+    
     var audio = new Audio("sounds/tom-1.mp3");
-    audio.play(); //adds sound to the drums when clicked
+    audio.play();                                          //adds sound to the drums when clicked
+
+
+
+
+    
 });
 
 }
