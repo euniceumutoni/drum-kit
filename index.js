d2d3ce8254a330990dcfc4cd2f4d197ba3cd9ec5
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-01-26 23:51:10 -0600

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ for (var i = 0; i <numberOfDrumButtons; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     alert ("I got clicked!");                              //pop up on page when clicked
     
-    this.style.color = "white";                            // changes the clicked drums white
+    this.style.color = "green";                            // changes the clicked drums green
     
     var audio = new Audio("sounds/tom-1.mp3");
     audio.play();                                          //adds sound to the drums when clicked
