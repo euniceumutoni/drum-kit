3a9e3c37648decccb525b903c8f9e2eefec96ec3
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-01-26 23:52:06 -0600

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i <numberOfDrumButtons; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert ("I got clicked!");                              //pop up on page when clicked
+    //alert ("I got clicked!");                              //pop up on page when clicked
     
     this.style.color = "green";                            // changes the clicked drums green
     
