7a995b3172c17788697288f93a46c95a55010e4a
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-01-26 23:45:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ for (var i = 0; i <numberOfDrumButtons; i++) {
 document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     alert ("I got clicked!");
     //add more funciton uses
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
 });
 
 }
